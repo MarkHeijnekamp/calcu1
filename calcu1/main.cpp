b91f856b7200efa092d32f39f84a3ab41ddd21cb
Mark Heijnekamp <26150@ma-web.nl>
2018-10-05 23:05:15 +0200

    Initial Commit

---
+++ calcu1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  calcu1
+//
+//  Created by mark heijnekamp on 05/10/2018.
+//  Copyright © 2018 mark heijnekamp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
